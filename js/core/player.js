66aa0ca5fc9041f3a0f822f2f0414d0826e48fb3
gotankersley <gotankersley@harding.edu>
2014-02-06 21:48:32 -0600

    Player selection

--- js/core/player.js
+++ js/core/player.js
@@ -2,7 +2,7 @@ var PLAYER_HUMAN = 0;
 var PLAYER_RANDOM = 1;
 var PLAYER_SIM = 2;
 var PLAYER_MCTS = 3;
-
+var PLAYERS = ['Human', 'Random', 'Sim', 'MCTS'];
 
 //Class Player
 function Player(currentGame, board, player1Type, player2Type) {
--- js/main.js
+++ js/main.js
@@ -21,7 +21,11 @@ $(function() {
         
     });
     
-    $('#players select').change(function() {
+    //Dynamically populate player selection menus
+    for (var i in PLAYERS) {
+        $('.player').append('<option value="' + i + '">' + PLAYERS[i] + '</option>');
+    }    
+    $('.player').change(function() {
         var playerNum = ($(this).attr('id') == 'player1')? PLAYER1 : PLAYER2;
         var playerType = Number($(this).val());
         game.player.set(playerNum, playerType);
