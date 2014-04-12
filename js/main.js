5fd7099ed010bba8f3c054a09325a2a91786fd54
Glen Tankersley <gotankersley@harding.edu>
2014-04-11 22:35:01 -0500

    Updated latest stable release

--- js/main.js
+++ js/main.js
@@ -11,13 +11,21 @@ $(function() {
     $('.game-option').change(function() {
         var opt = $(this).val();
         if ($(this).attr('name') == 'speed') SETTING_ROT_SPEED = opt;
+		else if ($(this).attr('name') == 'abdepth') {
+			if (opt <= 0) opt =1;
+			else if (opt > 3) {
+				alert('Maximum depth is 3');
+				opt = 3;
+			}
+			ALPHA_BETA_MAX_DEPTH = opt;
+		}
         else if (opt == 0) SETTING_ROT_ANIM = !SETTING_ROT_ANIM;
         else if (opt == 1) SETTING_ROW_NUMBERS = !SETTING_ROW_NUMBERS;
         else if (opt == 2) SETTING_ROW_NOTATION = !SETTING_ROW_NOTATION;
         else if (opt == 3) {
             SETTING_FIND_WINS = !SETTING_FIND_WINS;
             $('#find-wins').toggle();
-        }        
+        }        		
         
     });
     
