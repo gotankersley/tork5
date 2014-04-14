d45928640a6319fc87bd274341dede9839ccdc91
Glen Tankersley <gotankersley@harding.edu>
2014-04-14 17:20:29 -0500

    Added mcts player back - going to fix it for real this time

--- js/constants/player.js
+++ js/constants/player.js
@@ -2,9 +2,9 @@ var PLAYER_HUMAN = 0;
 var PLAYER_ALPHA_BETA = 1;
 var PLAYER_RANDOM = 2;
 var PLAYER_MC = 3;
-var PLAYER_SIM = 4;
-var PLAYER_CPP = 5;
-var PLAYER_MCTS = 6;
-var PLAYERS = ['Human', 'A/B', 'Random', 'MC']; //, 'Sim', 'C++', 'MCTS'];
+var PLAYER_MCTS = 4;
+var PLAYER_SIM = 5;
+var PLAYER_CPP = 6;
+var PLAYERS = ['Human', 'A/B', 'Random', 'MC', 'MCTS']; //, 'Sim', 'C++'];
 
 var INFINITY = 1000000;
